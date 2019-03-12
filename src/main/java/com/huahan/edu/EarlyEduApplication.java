1b67d84a4d8d617049db4fdb3f10964d236ae42f
jiangtan <407874382@qq.com>
2019-03-12 13:54:43 +0800

    Create Project

---
+++ src/main/java/com/huahan/edu/EarlyEduApplication.java
@@ -0,0 +1,13 @@
+package com.huahan.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyEduApplication.class, args);
+	}
+
+}
